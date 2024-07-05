9f7195ef9501ef93e3ad6396bcae9fc27fa2f9be
Alroy <alroypereira05@gmail.com>
2024-07-05 22:54:32 +0530

    Fixed mobile navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -44,21 +44,23 @@ const Navbar = () => {
       <ul
         className={
           nav
-            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] ease-in-out duration-500'
+            ? 'fixed md:hidden left-0 top-0 w-[60%] h-full border-r border-white bg-white ease-in-out duration-500'
             : 'ease-in-out w-[60%] duration-500 fixed top-0 bottom-0 left-[-100%]'
         }
       >
         {/* Mobile Logo */}
-        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>REACT.</h1>
+        <h1 className='w-full text-3xl font-bold text-[#00df9a] m-4'>Arizon</h1>
 
         {/* Mobile Navigation Items */}
         {navItems.map(item => (
-          <li
-            key={item.id}
-            className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer border-gray-600'
-          >
-            {item.text}
-          </li>
+          <Link to={item.link}>
+            <li
+              key={item.id}
+              className='p-4 border-b rounded-xl hover:bg-[#00df9a] duration-300 hover:text-black cursor-pointer'
+            >
+              {item.text}
+            </li>
+          </Link>
         ))}
       </ul>
     </div>
